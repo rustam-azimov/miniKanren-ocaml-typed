46ff472314f1e5905153ef72ef3deaf07ba8e907
Rustam Azimov <rustam.azimov19021995@gmail.com>
2016-06-16 00:04:16 +0300

    Added appendo.

--- regression/test0HG.ml
+++ regression/test0HG.ml
@@ -26,7 +26,16 @@ let rec uniono l s ls =
       ])
  ]
 
-(*let rec prependo (x : string logic) (l : string llist) (xl : string llist) =
+let rec appendo l s ls =
+ conde [
+   (l === !Nil) &&& (s === ls);
+   fresh (x l' ls')
+      (l === x % l')
+      (ls === x % ls')
+      (appendo l' s ls')
+ ]
+
+(*let rec prependo (x : char llist) (l : (char llist) llist) (xl : (char llist) llist) =
  *conde [
  *  (l === !Nil) &&& (xl === !Nil);
  *  fresh (y l' xl')
@@ -34,7 +43,6 @@ let rec uniono l s ls =
  *     (uniono (of_list [x ^ y]) xl' xl)
  *     (prependo x l' xl')
  *]
- *
  *
  *let rec concato l s ls =
  *conde [
@@ -45,7 +53,8 @@ let rec uniono l s ls =
  *     (uniono xs ls' ls)
  *     (concato l' s ls')
  *]
-*)
+ *)
+
 
 let _ =
   (*containo tests*)
@@ -53,8 +62,13 @@ let _ =
   run1 ~n:10  (REPR (fun q -> containo q (of_list [1]) !false ) );
   
   (*uniono tests*)
-  run1 ~n:1  (REPR (fun q -> uniono !Nil (of_list [1]) q ) );
-  run1 ~n:1  (REPR (fun q -> uniono q (of_list [1]) (of_list [2;3;1]) ) );
+  run1 ~n:1  (REPR (fun q -> uniono (of_list [1]) (of_list [1]) q ) );
+  run1 ~n:2  (REPR (fun q -> uniono q (of_list [1]) (of_list [2;3;1]) ) );
   run1 ~n:1  (REPR (fun q -> uniono (of_list [1]) q (of_list [1;2;3]) ) );
+
+  (*appendo tests*)
+  run1 ~n:1  (REPR (fun q -> appendo !Nil (of_list [1]) q ) );
+  run1 ~n:1  (REPR (fun q -> appendo q (of_list [1]) (of_list [2;3;1]) ) );
+  run1 ~n:1  (REPR (fun q -> appendo (of_list [1]) q (of_list [1;2;3]) ) );
 
   ()
